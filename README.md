74e1a96f037e05b0918dfd80c0920bdf97507ed7
pc <pc@pc-Bravo-15-A4DCR>
2022-12-07 10:43:17 +0700

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Platform Frappe
+
+Platform Frappe is an awesome and flexible toolkit for building ERPNext or any platform through frappe framework.
+
+#### License
+
+MIT
---
+++ platform_frappe/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Platform Frappe",
+			"type": "module",
+			"label": _("Platform Frappe")
+		}
+	]
